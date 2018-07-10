2504c28dc3020ceafbd2f69581a37c73d151db63
balintt21 <balintt21@gmail.com>
2018-07-10 20:43:27 +0200

    Update: recompile and relink if flags have been changed, the need of recompilation is decided by modification dates ,strip is selective

--- builder.sh
+++ builder.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-if [ -z "$builder_FLAGS" ]; then
-    builder_FLAGS="-O3 -Wall"
+if [ -z "$builder_CROSS_COMPILE" ]; then
+    builder_CROSS_COMPILE=""
+fi
+if [ -z "$builder_CC_FLAGS" ]; then
+    builder_CC_FLAGS="-O3 -Wall"
+fi
+if [ -z "$builder_CXX_FLAGS" ]; then
+    builder_CXX_FLAGS="-O3 -Wall"
+fi
+if [ -z "$builder_AS_FLAGS" ]; then
+    builder_AS_FLAGS=""
 fi
 if [ -z "$builder_CC" ]; then
-    builder_CC=g++
+    builder_CC="${builder_CROSS_COMPILE}gcc"
+fi
+if [ -z "$builder_CXX" ]; then
+    builder_CXX="${builder_CROSS_COMPILE}g++"
+fi
+if [ -z "$builder_AS" ]; then
+    builder_AS="${builder_CROSS_COMPILE}as"
 fi
 if [ -z "$builder_AR" ]; then
-    builder_AR=ar
+    builder_AR="${builder_CROSS_COMPILE}ar"
 fi
 if [ -z "$builder_LD_FLAGS" ]; then
     builder_LD_FLAGS=""
@@ -15,21 +30,25 @@ fi
 if [ -z "$builder_OUTPUT_NAME" ]; then
     builder_OUTPUT_NAME="output"
 fi
-if [ ! -z "$2" ]; then
+if [ -n "$2" ]; then
     builder_OUTPUT_NAME="$2"
 fi
 if [ -z "$builder_NAMESPACE" ]; then
     builder_NAMESPACE="$builder_OUTPUT_NAME"
 fi
-if [ ! -z "$3" ]; then
+if [ -n "$3" ]; then
     builder_NAMESPACE="$3"
 fi
 
+build_type=""
 exclude_file=""
+has_cc="$(type "$builder_CC" &> /dev/null)"
+has_as="$(type "$builder_AS" &> /dev/null)"
+there_are_cpp_files=""
 
 gather_includes()
 {
-    if [ ! -d "include" ]; then
+    if [ ! -d "include/$builder_NAMESPACE" ]; then
         mkdir -p "include/$builder_NAMESPACE"
     else
         rm -r "include/$builder_NAMESPACE"
@@ -69,6 +88,46 @@ check_if_excluded()
     return 0
 }
 
+compile()
+{
+    if [[ $1 == *.c ]] && [ -n "$has_cc" ]; then
+        compiler="$builder_CC"
+        flags="$builder_CC_FLAGS"
+    elif { [[ $1 == *.s ]] || [[ $1 == *.S ]]; } && [ -n "$has_as" ]; then
+        compiler="$builder_AS"
+        flags="$builder_AS_FLAGS"
+    else
+        compiler="$builder_CC"
+        flags="$builder_CXX_FLAGS"
+        there_are_cpp_files="1"
+    fi
+
+    if [ -n "$build_type" ]; then
+        flags="-fPIC $flags"
+    fi
+
+    $compiler $flags -c "$1" -o "$2"
+}
+
+linking()
+{
+    if [ $there_are_cpp_files ]; then
+        linker="$builder_CXX"
+    else
+        linker="$builder_CC"
+    fi
+    
+    if [ -z "$2" ]; then
+        echo "Linking with $linker $builder_LD_FLAGS"
+        echo "$1"
+        $linker $builder_LD_FLAGS -o "output/$builder_OUTPUT_NAME" $1
+    else
+        echo "Linking with $linker $2 $builder_LD_FLAGS"
+        echo "$1"
+        $linker $2 $builder_LD_FLAGS -o "output/lib/lib$builder_OUTPUT_NAME.so" $1
+    fi
+}
+
 build()
 {
     if [ ! -d "build" ]; then
@@ -78,52 +137,65 @@ build()
     select_exclude_file "$1"
 
     build_type="$1"
-    prev_build_type=""
-    number_of_changes=0
+    build_dsc="$build_type:$builder_CC_FLAGS:$builder_CXX_FLAGS:$builder_AS_FLAGS:$builder_LD_FLAGS"
+    build_changed=0
+    compiled_at_leat_one_file=0
     source_files="$(find $(pwd)/src -type f,l -iregex '.*\.\(c\|i\|ii\|cc\|cp\|cxx\|cpp\|CPP\|c++\|C\|s\|S\|sx\)' -printf 'src/%P ')"
 
-    if [ "$1" == "dynamic" ]; then
-        builder_FLAGS="$builder_FLAGS -fPIC"
+    if [ -f "build/build.dsc" ]; then
+        prev_build_dsc="$(cat build/build.dsc)"
+        if [ "$build_dsc" != "$prev_build_dsc" ]; then build_changed=1; fi;
+        if [ $build_changed -eq 1 ]; then
+            echo "Rebuilding due to type or compile/linking flag changes."
+        fi
+    else
+        build_changed=1
     fi
-
-    if [ -f "build/build.type" ]; then
-        prev_build_type=$(cat build/build.type)
-    fi
-
-    echo -n "$build_type" > build/build.type
+    echo -n "$build_dsc" > "build/build.dsc"
 
     IFS=' ' read -r -a sources <<< "$source_files"
-    echo "Compiling with: $builder_CC $builder_FLAGS"
+    echo -e "Compiling:\n C with: $builder_CC $builder_CC_FLAGS\n C++ with: $builder_CXX $builder_CXX_FLAGS\n Assembler with: $builder_AS $builder_AS_FLAGS\n"
 
     for source_file in "${sources[@]}"
     do
         check_if_excluded "$(echo $source_file | cut -d'/' -f2-)"
-        if [ "$?" == "1" ]; then
+        if [ $? == 1 ]; then
             echo -e "\t$source_file -> EXCLUDED"
             continue
         fi
-        source_file_name=$(echo "$source_file" | cut -d'.' -f1)
-        source_file_name=${source_file##*/}
-        source_file_checksum=$($builder_CC $builder_FLAGS  -E $source_file -o - | md5sum  | cut -d' ' -f1)
-        prev_checksum_of_source_file=""
 
-        if [ -f "build/$source_file_name.md5" ]; then
-            prev_checksum_of_source_file=$(cat build/"$source_file_name".md5)
+        source_file_name=$(echo "$source_file" | cut -d'.' -f1)
+        source_file_name=${source_file_name##*/}
+        object_file_path="build/$source_file_name.o"
+        file_has_changed=""
+
+        if [ -f "$object_file_path" ]; then
+            make_rule_of_dependencies=$($builder_CC $builder_FLAGS  -MM "$source_file" | cut -d':' -f2- )
+            IFS=' ' read -r -a files_to_check <<< "$make_rule_of_dependencies"
+            for file_to_check in "${files_to_check[@]}"
+            do
+                if [ $(stat -c %Y "$object_file_path") -lt $(stat -c %Y "$file_to_check") ]; then
+                    file_has_changed="1"
+                    break  
+                fi
+            done
+        else
+            file_has_changed="1"
         fi
 
-        if [ "$source_file_checksum" != "$prev_checksum_of_source_file" ] || [ "$build_type" != "$prev_build_type" ]; then
-            echo -e "\t$source_file -> $source_file_name.o"
-            $builder_CC $builder_FLAGS -c "$source_file" -o "build/$source_file_name.o"
+        if [ $file_has_changed ] || [ $build_changed -eq 1 ]; then
+            echo -e "\t$source_file -> $object_file_path"
+            compile "$source_file" "$object_file_path"
 
             if [ $? -ne 0 ]; then
                 echo "Build failed!"
                 exit 1
             fi
 
-            number_of_changes=$((number_of_changes+1))
-            echo -n "$source_file_checksum" > "build/$source_file_name.md5"
+            compiled_at_leat_one_file=1
         fi
     done
+    echo ""
 }
 
 gather_object_files()
@@ -146,7 +218,7 @@ if [ -z $1 ] || [ "$1" == "help" ]; then
 "\tdynamic\t\t\t- Build dynamic library\n"\
 "\texec\t\t\t- Build executable\n"\
 "\tclean\t\t\t- Clean build results\n"\
-"\tstrip\t\t\t- Strips any executable or dynamic library from ./output\n"\
+"\tstrip\t\t\t- Strips any executable or dynamic library from ./output (see strip --help)\n"\
 "\thelp\t\t\t- Show this help\n"\
 "Environment variables\n"\
 "\tbuilder_FLAGS\t\t- compiler flags (default: -O3 -Wall)\n"\
@@ -173,20 +245,17 @@ elif [ "$1" == "dynamic" ]; then
     echo "Dynamic build."
 
     gather_includes
-    build $1
+    build "$1"
 
-    echo -n "dynamic" > build/build.type
-
-    if [ "$number_of_changes" != "0" ]; then
+    if [ $compiled_at_leat_one_file -ne 0 ]; then
         if [ ! -d "output/lib" ]; then
           mkdir -p output/lib
         fi
         
 	    gather_object_files
 
-	    echo "Linking with $builder_CC."
-        echo $object_files
-	    $builder_CC -shared $builder_LD_FLAGS -o "output/lib/lib$builder_OUTPUT_NAME.so" $object_files
+	    linking "$object_files" "-shared"
+
 	    if [ $? -ne 0 ]; then
 	        echo "Build failed!"
 	        exit 1
@@ -199,11 +268,9 @@ elif [ "$1" == "static" ]; then
     echo "Static build."
     
     gather_includes
-    build
+    build "$1"
 
-    echo -n "static" > build/build.type
-
-    if [ "$number_of_changes" != "0" ]; then
+    if [ $compiled_at_leat_one_file -ne 0 ]; then
         if [ ! -d "output/lib" ]; then
           mkdir -p output/lib
         fi
@@ -221,20 +288,17 @@ elif [ "$1" == "exec" ]; then
     echo "Building executable."
 
     gather_includes
-    build $1
+    build "$1"
 
-    echo -n "executable" > build/build.type
-
-    if [ "$number_of_changes" != "0" ]; then
+    if [ $compiled_at_leat_one_file -ne 0 ]; then
         if [ ! -d "output" ]; then
           mkdir -p output
         fi
         
 	    gather_object_files
 
-	    echo "Linking with $builder_CC."
-        echo $object_files
-	    $builder_CC $builder_LD_FLAGS -o "output/$builder_OUTPUT_NAME" $object_files
+	    linking "$object_files"
+
 	    if [ $? -ne 0 ]; then
 	        echo "Build failed!"
 	        exit 1
@@ -257,21 +321,45 @@ elif [ "$1" == "clean" ]; then
         rm -r include
     fi
 elif [ "$1" == "strip" ]; then
-    #TODO use find
-    to_strip="output/$builder_OUTPUT_NAME"
-    if [ -f "$to_strip" ]; then
-        echo "strip --strip-unneeded $to_strip"
-        strip --strip-unneeded "$to_strip"
-    fi
-    to_strip="output/lib/lib$builder_OUTPUT_NAME.so"
-    if [ -f "$to_strip" ]; then
-        echo "strip --strip-unneeded $to_strip"
-        strip --strip-unneeded "$to_strip"
-    fi
-    to_strip="output/lib/lib$builder_OUTPUT_NAME.dll"
-    if [ -f "$to_strip" ]; then
-        echo "strip --strip-unneeded $to_strip"
-        strip --strip-unneeded "$to_strip"
+    if [ -z "$2" ] || [[ $2 == *-h* ]]; then
+        echo -e "Usage: $0 $1 [OPTIONS]\n"\
+"Options\n"\
+"-h, --help\t - Show this help\n"\
+"-a, --all\t - Strips all type of outputs(dynamic,static,exec)\n"\
+"-s\t\t - Strips static library files\n"\
+"-d\t\t - Strips dynamic library files\n"\
+"-e\t\t - Strips executable output files\n"
+    else
+        if [[ $2 == *-a* ]]; then
+            all_files_to_strip="$(find $(pwd)/output -type f -printf '%P ')"
+        else
+            regex=""
+            if [[ $2 == *s* ]]; then
+                regex="${regex}a\|"
+            fi
+            if [[ $2 == *d* ]]; then
+                regex="${regex}so\|dll\|"
+            fi
+
+            if [ -n "$regex" ]; then
+                regex=${regex%?}
+                regex=".*\.\($regex)"
+                echo $regex
+                all_files_to_strip="$(find $(pwd)/output/lib -type f -iregex $regex -printf 'lib/%P ')"
+            fi
+            
+            if [[ $2 == *e* ]]; then
+                exec_files=$(ls -p output | grep -v / | tr '\n' ' ')
+                all_files_to_strip="$all_files_to_strip $exec_files"
+            fi
+        fi
+
+        IFS=' ' read -r -a files_to_strip <<< "$all_files_to_strip"
+        for file_to_strip in "${files_to_strip[@]}"
+        do
+            echo "strip --strip-unneeded $file_to_strip"
+            strip --strip-unneeded "output/$file_to_strip"
+        done
     fi
 else
     echo "Unrecognized parameter: \"$1\"!"
